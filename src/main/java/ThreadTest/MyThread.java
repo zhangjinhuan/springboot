2c31bfc7f8fcec43c35a8508cef7088daa86d94a
zhangjinhuan <694328897@qq.com>
2019-05-17 13:42:12 +0800

    延延长线程暂停时间

--- src/main/java/ThreadTest/MyThread.java
+++ src/main/java/ThreadTest/MyThread.java
@@ -8,7 +8,7 @@ public class MyThread extends Thread{
     public void run(){
         for(int i = 0;i < 100;i++){
             try {
-                Thread.sleep(10);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
